9fee14c20f700c7fa296ac8f9d283e1b4ae61bbc
Julian Sobczuk <julek@sobczuk.pl>
2025-05-08 13:24:46 +0200

    basic logic

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import express from "express";
+const app = express();
+const port = 2537;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
